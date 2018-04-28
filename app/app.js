6f770eeaca8861be385c88cb5f19e79faa32dfdb
Alfredo <oxarbitrage@gmail.com>
2018-04-28 10:59:33 -0300

    initial website

---
+++ app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.home',
+  'myApp.about',
+  'myApp.contact',
+  'angularTypewrite',
+  'myApp.i18n'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/home'});
+}]);
